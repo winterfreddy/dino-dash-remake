36f01bb312dc299ee5afa02635a5b9c960df45b6
Winfred Huang <huang.winfred21@gmail.com>
2021-08-29 23:59:32 -0700

    Add full screen dropdown for splash page

---
+++ src/layout.js
@@ -0,0 +1,7 @@
+function openSplashPage() {
+    document.getElementById("splashPage").style.height = "100%";
+}
+
+function closeSplashPage() {
+    document.getElementById("splashPage").style.height = "0%";
+}
